9acb9eb97f867ddc355f38a34524a2c51a2e9393
saifulemon <463saiful0@gmail.com>
2022-04-29 12:18:15 +0600

    Clients slider section added

--- assets/js/core.js
+++ assets/js/core.js
@@ -8,3 +8,39 @@ window.onscroll = function () {
         nav.classList.remove('header-scrolled');
     }
 }
+
+
+// Owl Carousel
+$(document).ready(function() {
+    $('.client-slider-section').owlCarousel({
+        items: 4,
+        loop:true,
+        nav: false,
+        autoplay: true,
+        autoplayTimeoute: 2000,
+        autoplayHoverPause: true,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:2
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:6
+            }
+        }
+    });
+});
+
+
+// Nav hide
+let nvaBar = document.querySelectorAll('.nav-link');
+let navCollapse = document.querySelector('.navbar-collapse.collapse');
+
+nvaBar.forEach(function(a) {
+    a.addEventListener('click', function(){
+        navCollapse.classList.remove('show');
+    });
+});
